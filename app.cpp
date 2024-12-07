1c4787e2fd68a827201bcc7cac482b49167069da
Tuominiemi William <william.tuominiemi@aalto.ficd>
2024-12-08 00:40:36 +0200

    remove task

--- app.cpp
+++ app.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <stdlib.h>
+#include <vector>
 using namespace std;
 
 void write(string entry)
@@ -25,9 +26,11 @@ void readTasks()
     ifstream infile("tasks.txt");
     if (infile.is_open())
     {
+        int lineNumber = 1;
         while (getline(infile, line))
         {
-            cout << line << '\n';
+            cout << lineNumber << ". " << line << '\n';
+            lineNumber++;
         }
         infile.close();
     }
@@ -45,11 +48,61 @@ void addTask()
     write(x);
 }
 
+void removeTask()
+{
+    vector<string> tasks;
+    string line;
+
+    ifstream infile("tasks.txt");
+    if (infile.is_open())
+    {
+        while (getline(infile, line))
+        {
+            tasks.push_back(line);
+        }
+        infile.close();
+    }
+    else
+    {
+        cout << "Unable to open file for reading\n";
+        return;
+    }
+
+    readTasks();
+
+    int x;
+    cout << "Which task would you like to remove? Enter task number: ";
+    cin >> x;
+
+    if (x < 1 || x > tasks.size())
+    {
+        cout << "Invalid task number!\n";
+        return;
+    }
+
+    tasks.erase(tasks.begin() + x - 1);
+
+    ofstream outfile("tasks.txt");
+    if (outfile.is_open())
+    {
+        for (const string &task : tasks)
+        {
+            outfile << task << '\n';
+        }
+        outfile.close();
+        cout << "Task removed successfully.\n";
+    }
+    else
+    {
+        cout << "Unable to open file for writing\n";
+    }
+}
+
 int main()
 {
     while (true)
     {
-        cout << "(1) Add a new task (2) Read all tasks (0) Quit \n";
+        cout << "(1) Add a new task (2) Read all tasks (3) Remove a task (0) Quit \n";
 
         int x;
         cout << "Enter you option: ";
@@ -66,6 +119,10 @@ int main()
         else if (x == 2)
         {
             readTasks();
+        }
+        else if (x == 3)
+        {
+            removeTask();
         }
     }
 
